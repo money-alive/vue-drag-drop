5c2b7c78c6f7f4e490caeca309e490b0824c7cf2
Daniel Stuessy <danielstuessy@gmail.com>
2021-11-14 11:23:33 +0000

    vue file indentation

--- src/Drag.vue
+++ src/Drag.vue
@@ -16,87 +16,87 @@
 </template>
 
 <script>
-	import transferDataStore from './transferDataStore';
-	import { dropEffects, effectsAllowed, events } from './constants';
+import transferDataStore from './transferDataStore';
+import { dropEffects, effectsAllowed, events } from './constants';
 
-	export default {
-		props: {
-			draggable: { type: Boolean, default: true },
-			transferData: {},
-			dropEffect: { validator: (value) => value in dropEffects },
-			effectAllowed: { validator: (value) => value in effectsAllowed },
-			image: String,
-			imageXOffset: { type: Number, default: 0 },
-			imageYOffset: { type: Number, default: 0 },
-			hideImageHtml: { type: Boolean, default: true },
-			tag: { type: String, default: 'div' },
+export default {
+	props: {
+		draggable: { type: Boolean, default: true },
+		transferData: {},
+		dropEffect: { validator: (value) => value in dropEffects },
+		effectAllowed: { validator: (value) => value in effectsAllowed },
+		image: String,
+		imageXOffset: { type: Number, default: 0 },
+		imageYOffset: { type: Number, default: 0 },
+		hideImageHtml: { type: Boolean, default: true },
+		tag: { type: String, default: 'div' },
+	},
+	data() {
+		return { dragging: false };
+	},
+	computed: {
+		events: () => events,
+		scopedData() {
+			return this.dragging && this.transferData;
 		},
-		data() {
-			return { dragging: false };
-		},
-		computed: {
-			events: () => events,
-			scopedData() {
-				return this.dragging && this.transferData;
-			},
-			hideImageStyle: () => ({ position: 'fixed', top: '-1000px' }),
-		},
-		methods: {
-			emitEvent(name, nativeEvent) {
-				const transfer = nativeEvent.dataTransfer;
+		hideImageStyle: () => ({ position: 'fixed', top: '-1000px' }),
+	},
+	methods: {
+		emitEvent(name, nativeEvent) {
+			const transfer = nativeEvent.dataTransfer;
 
-				// Set drop effect on dragenter and dragover
-				if ([events.dragenter, events.dragover].includes(name)) {
-					if (this.dropEffect) {
-						transfer.dropEffect = this.dropEffect;
+			// Set drop effect on dragenter and dragover
+			if ([events.dragenter, events.dragover].includes(name)) {
+				if (this.dropEffect) {
+					transfer.dropEffect = this.dropEffect;
+				}
+			}
+
+			// A number of things need to happen on drag start
+			if (name === events.dragstart) {
+				// Set the allowed effects
+				if (this.effectAllowed) {
+					transfer.effectAllowed = this.effectAllowed;
+				}
+
+				// Set the drag image
+				if (this.image || this.$slots.image) {
+					let image;
+					if (this.image) {
+						image = new Image();
+						image.src = this.image;
+					} else if (this.$slots.image) {
+						image = this.$slots.image[0].elm;
+					}
+					if (transfer.setDragImage) {
+						transfer.setDragImage(image, this.imageXOffset, this.imageYOffset);
 					}
 				}
 
-				// A number of things need to happen on drag start
-				if (name === events.dragstart) {
-					// Set the allowed effects
-					if (this.effectAllowed) {
-						transfer.effectAllowed = this.effectAllowed;
-					}
-
-					// Set the drag image
-					if (this.image || this.$slots.image) {
-						let image;
-						if (this.image) {
-							image = new Image();
-							image.src = this.image;
-						} else if (this.$slots.image) {
-							image = this.$slots.image[0].elm;
-						}
-						if (transfer.setDragImage) {
-							transfer.setDragImage(image, this.imageXOffset, this.imageYOffset);
-						}
-					}
-
-					// Set the transfer data
-					if (this.transferData !== undefined) {
-						transferDataStore.data = this.transferData;
-						// Set a dummy string for the real transfer data. Not actually used
-						// for anything, but necesssary for browser compatibility.
-						//
-						// TODO: Maybe this should be the actual data serialized. But since
-						// it's not actually used for anything it seems like a waste of CPU.
-						nativeEvent.dataTransfer.setData('text', '');
-					}
-
-					// Indicate that we're dragging.
-					this.dragging = true;
+				// Set the transfer data
+				if (this.transferData !== undefined) {
+					transferDataStore.data = this.transferData;
+					// Set a dummy string for the real transfer data. Not actually used
+					// for anything, but necesssary for browser compatibility.
+					//
+					// TODO: Maybe this should be the actual data serialized. But since
+					// it's not actually used for anything it seems like a waste of CPU.
+					nativeEvent.dataTransfer.setData('text', '');
 				}
 
-				// At last, emit the event.
-				this.$emit(name, this.transferData, nativeEvent);
+				// Indicate that we're dragging.
+				this.dragging = true;
+			}
 
-				// Clean up stored data on drag end after emitting.
-				if (name === events.dragend) {
-					transferDataStore.data = undefined;
-					this.dragging = false;
-				}
-			},
+			// At last, emit the event.
+			this.$emit(name, this.transferData, nativeEvent);
+
+			// Clean up stored data on drag end after emitting.
+			if (name === events.dragend) {
+				transferDataStore.data = undefined;
+				this.dragging = false;
+			}
 		},
-	};
+	},
+};
 </script>
--- src/Drop.vue
+++ src/Drop.vue
@@ -10,55 +10,55 @@
 </template>
 
 <script>
-	import transferDataStore from './transferDataStore';
-	import { events } from './constants';
+import transferDataStore from './transferDataStore';
+import { events } from './constants';
 
-	const insideElements = new Set();
+const insideElements = new Set();
 
-	export default {
-		data() {
-			return { transferData: undefined, isDraggingOver: false };
+export default {
+	data() {
+		return { transferData: undefined, isDraggingOver: false };
+	},
+	props: {
+		tag: { type: String, default: 'div' },
+	},
+	computed: {
+		events: () => events,
+		scopedData() {
+			return this.isDraggingOver && this.transferData;
 		},
-		props: {
-			tag: { type: String, default: 'div' },
-		},
-		computed: {
-			events: () => events,
-			scopedData() {
-				return this.isDraggingOver && this.transferData;
-			},
-		},
-		methods: {
-			emitEvent(name, nativeEvent) {
-				this.transferData = transferDataStore.data;
-				this.$emit(name, this.transferData, nativeEvent);
+	},
+	methods: {
+		emitEvent(name, nativeEvent) {
+			this.transferData = transferDataStore.data;
+			this.$emit(name, this.transferData, nativeEvent);
 
-				/**
-				 * After emitting the event, we need to determine if we're still
-				 * dragging inside this Drop. We keep a Set of all elements that we've
-				 * dragged into, then clear the data if that set is empty.
-				 */
+			/**
+			 * After emitting the event, we need to determine if we're still
+			 * dragging inside this Drop. We keep a Set of all elements that we've
+			 * dragged into, then clear the data if that set is empty.
+			 */
 
-				// Add to the set on dragenter.
-				if (name === events.dragenter) {
-					if (insideElements.size || nativeEvent.target === this.$el) {
-						insideElements.add(nativeEvent.target);
-					}
+			// Add to the set on dragenter.
+			if (name === events.dragenter) {
+				if (insideElements.size || nativeEvent.target === this.$el) {
+					insideElements.add(nativeEvent.target);
 				}
+			}
 
-				// Remove from the set on dragleave.
-				if (name === events.dragleave) {
-					insideElements.delete(nativeEvent.target);
-				}
+			// Remove from the set on dragleave.
+			if (name === events.dragleave) {
+				insideElements.delete(nativeEvent.target);
+			}
 
-				// A drop resets everything.
-				if (name === events.drop) {
-					insideElements.clear();
-				}
+			// A drop resets everything.
+			if (name === events.drop) {
+				insideElements.clear();
+			}
 
-				// Finally, since Vue can't react to Set changes, set a flag indicating drag status.
+			// Finally, since Vue can't react to Set changes, set a flag indicating drag status.
 				this.isDraggingOver = Boolean(insideElements.size);
-			},
 		},
-	};
+	},
+};
 </script>
